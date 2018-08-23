4f48b1a942755779d822b00dbe17e41b0be711f6
Robert Leedy <leedyrj@gmail.com>
2018-08-23 13:07:42 -0500

    finished through square root problem

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            // Find the words in the collection that start with the letter 'L'
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                                          where fruit.StartsWith("L")
+                                          orderby fruit
+                                          select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine($"{fruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0).OrderBy(n => n);
+
+
+            foreach (int number in fourSixMultiples)
+            {
+                Console.WriteLine($"{number}");
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            IEnumerable<string> descend = from alph in names
+                                          orderby alph descending
+                                          select alph;
+
+            foreach (string n in descend)
+            {
+                Console.WriteLine($"{n}");
+            }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbs = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> descending = from n in numbs
+                                          orderby n ascending
+                                          select n;
+
+            foreach (int n in descending)
+            {
+                Console.WriteLine($"{n}");
+            }
+
+            // Output how many numbers are in this list
+            IEnumerable<int> manyNumbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            int count = manyNumbers.Count();
+
+            Console.WriteLine(count);
+
+            // How much money have we made?
+            IEnumerable<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double total = purchases.Sum();
+            Console.WriteLine($"total {total}");
+
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            Console.WriteLine(prices.Max());
+
+
+
+        }
+    }
+}
